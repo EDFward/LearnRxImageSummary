bea1fb0eace16016713955caf9ddc4ec3b2dd78c
EDFward <edfward@gmail.com>
2016-01-09 22:24:35 -0500

    Display image and show summary in console
    Finish the skeleton of pipeline, from the pic URL to the actual picture
    and analysis results.

--- RxImageSummary/AppDelegate.swift
+++ RxImageSummary/AppDelegate.swift
@@ -24,7 +24,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   func applicationWillTerminate(aNotification: NSNotification) {
     // Insert code here to tear down your application
   }
-
-
 }
 
--- RxImageSummary/UploadImageViewController.swift
+++ RxImageSummary/UploadImageViewController.swift
@@ -9,10 +9,11 @@
 import Cocoa
 import ReactiveCocoa
 
-
 class UploadImageViewController: NSViewController {
   
-  let (uploadSignal, uploadObserver) = Signal<Void, NoError>.pipe()
+  let (uploadSignal, uploadObserver) = Signal<Void, NSError>.pipe()
+
+  let apiInfo = NSDictionary(contentsOfFile: NSBundle.mainBundle().pathForResource("APIKey", ofType: "plist")!)!
 
   @IBAction func uploadPic(sender: NSButton) {
     uploadObserver.sendNext()
@@ -20,11 +21,27 @@ class UploadImageViewController: NSViewController {
 
   @IBOutlet weak var picView: NSImageView!
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
+  private func imageAnalysisRequest(imageData: NSData) -> NSURLRequest {
+    let apiKey = apiInfo["AlchemyAPIKey"]!
 
-    let imagePathSignal: Signal<NSURL?, NoError> = uploadSignal
-      .map { () -> NSURL? in
+    let urlString = [
+      "http://gateway-a.watsonplatform.net/calls/image/ImageGetRankedImageKeywords?",
+      "apikey=\(apiKey)&",
+      "imagePostMode=raw&",
+      "outputMode=json"].joinWithSeparator("")
+    let URL = NSURL(string: urlString)!
+    let request = NSMutableURLRequest(URL: URL)
+    request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+    request.HTTPMethod = "POST"
+    request.HTTPBody = imageData
+    return request
+  }
+
+  override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)!
+
+    let imageData: Signal<NSData?, NSError> = uploadSignal
+      .map { () in
         let openPanel = NSOpenPanel()
         openPanel.allowsMultipleSelection = false
         openPanel.canChooseFiles = true
@@ -33,24 +50,46 @@ class UploadImageViewController: NSViewController {
         openPanel.allowedFileTypes = ["jpg", "png", "jpeg"]
         // Block on file selection.
         if openPanel.runModal() == NSFileHandlingPanelOKButton {
-          return openPanel.URL
-        } else {
-          return nil
+          if let url = openPanel.URL {
+            return NSData(contentsOfURL:url)
+          }
         }
+        return nil
       }
       .filter { $0 != nil }
 
-    imagePathSignal.observeNext { url in
-      let imgData = NSData(contentsOfURL: url!)
-      if imgData != nil {
-        let img = NSImage(data: imgData!)
-        self.picView.image = img
-      } else {
-        print("Failed to get img from \(url)")
+    // TODO: Throttling, test flatten strategy, etc.
+
+    // Display the selected picture.
+    imageData.observeNext { data in
+      self.picView.image = NSImage(data: data!)
+    }
+
+    let analysisResults = imageData
+      .flatMap(FlattenStrategy.Latest) { (data: NSData?) -> SignalProducer<(NSData, NSURLResponse), NSError> in
+        // Must be non-nil after filtering.
+        let URLRequest = self.imageAnalysisRequest(data!)
+        return NSURLSession.sharedSession().rac_dataWithRequest(URLRequest)
       }
+      .map({ (data: NSData, URLResponse: NSURLResponse) -> String in
+        let result = String(data: data, encoding: NSUTF8StringEncoding)!
+        return result
+      })
+
+    // For now, simply output to console.
+    analysisResults.observeNext { res in
+      print(res)
     }
   }
   
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
   override func viewDidDisappear() {
     // Clear the signal.
     uploadObserver.sendCompleted()
